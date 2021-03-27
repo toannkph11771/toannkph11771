272de13cf996c09f9c4265eec094edf571392dde
abc <toannkph11771@fpt.edu.vn>
2021-03-27 10:25:47 +0700

    Initial commit

---
+++ call.js
@@ -0,0 +1,10 @@
+exports.tinhTong= function (a,b){
+    return a+b;
+}
+
+exports.tinhHieu = function (a,b){
+    return a-b;
+}
+exports.tinhTich =function (a,b){
+    return a*b;
+}
---
+++ index.js
@@ -0,0 +1,62 @@
+var http = require('http');
+var fs =require('fs');
+
+http.createServer(function (req, res){
+
+    res.writeHead(200, {'Content-Type' : 'text/html'});
+
+    // var tong = call.tinhTong(3,5);
+    //  var hieu = call.tinhHieu(12,9);
+    //  var tich = call.tinhTich(4,6);
+    //  res.end('Hello' + tong + hieu + tich);
+    var url = req.url;
+    if(url == '/'){
+       fs.readFile('index.html', function (erro, data){
+           if(erro == null){
+               res.write(data);
+               res.end();
+           }else {
+               res.end(erro);
+           }
+        });
+    }else if (url == '/insert'){
+        fs.writeFile('test.txt','Ghi vao file thu xem',function (erro){
+            if (erro == null){
+                res.end('ghi thanh cong');
+            }else {
+                res.end(erro);
+            }
+        })
+    }else if (url == '/append') {
+        fs.appendFile('test.txt','Ghi vao file lan 2',function (erro){
+            if (erro == null){
+                res.end('ghi thanh cong');
+            }else {
+                res.end(erro);
+            }
+        })
+    }else if (url =='/unlink'){
+
+        fs.unlink('test.txt',function (erro){
+            if (erro == null){
+                res.end('Xoa thanh cong');
+            }else {
+                res.end(erro);
+            }
+        })
+    }else if (url == '/rename'){
+
+        fs.rename('test.txt','text2.txt',function (erro){
+            if (erro == null){
+                res.end('Rename thanh cong');
+            }else {
+                res.end(erro);
+            }
+        })
+    }else {
+        res.end('404 not found');
+    }
+
+
+
+}).listen(8080);
